a3dd74668ec4d2c9b41baf6e96683543b79f7e96
Hypaspistai <shhegolkov1992@mail.ru>
2025-11-30 23:05:12 +0300

    commit

--- src/InterferenceExample.java
+++ src/InterferenceExample.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class InterferenceExample {
+/**
+ * Пример совместного использования ресурсов
+ */
+class InterferenceExample {
+    private static final int HUNDRED_MILLION = 100_000_000;
+    private AtomicInteger counter = new AtomicInteger();
+
+    boolean stop(){
+        return counter.incrementAndGet() > HUNDRED_MILLION;
+    }
+
+    void example() throws InterruptedException {
+        InterferenceThread thread1 = new InterferenceThread(this);
+        InterferenceThread thread2 = new InterferenceThread(this);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("Expected: " + HUNDRED_MILLION);
+        System.out.println("Result: " + thread1.getI());
+    }
 }
--- src/InterferenceThread.java
+++ src/InterferenceThread.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class InterferenceThread extends Thread {
+    private final InterferenceExample checker;
+    private static int i;
 
-public class InterferenceThread {
+    InterferenceThread(InterferenceExample checker) {
+        this.checker = checker;
+    }
+
+    private void increment() {
+        i++;
+    }
+
+    int getI() {
+        return i;
+    }
+
+    public void run() {
+        while (!checker.stop()) {
+            increment();
+        }
+    }
 }
